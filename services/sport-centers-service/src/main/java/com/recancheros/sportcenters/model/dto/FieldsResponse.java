701d666cb28c60d4acae2d1f08f7d20107c27809
Fabian Yate <fabianyate31@gmail.com>
2023-10-04 02:43:40 -0500

    Add dtos for fields, services and sports

---
+++ services/sport-centers-service/src/main/java/com/recancheros/sportcenters/model/dto/FieldsResponse.java
@@ -0,0 +1,15 @@
+package com.recancheros.sportcenters.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FieldsResponse {
+    private String name;
+    private String surface;
+    private String type;
+    private String availability;
+    private String price;
+
+}
---
+++ services/sport-centers-service/src/main/java/com/recancheros/sportcenters/model/dto/ServicesResponse.java
@@ -0,0 +1,10 @@
+package com.recancheros.sportcenters.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ServicesResponse {
+    private String name;
+}
