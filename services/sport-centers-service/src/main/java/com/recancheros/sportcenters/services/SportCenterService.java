70fd587a8b2b5ff9c6726f3d6fc10b8cf9d2749a
Fabian Yate <fabianyate31@gmail.com>
2023-10-04 02:46:08 -0500

    Add list of all SportCenters

--- services/sport-centers-service/src/main/java/com/recancheros/sportcenters/services/SportCenterService.java
+++ services/sport-centers-service/src/main/java/com/recancheros/sportcenters/services/SportCenterService.java
@@ -3,9 +3,11 @@ package com.recancheros.sportcenters.services;
 import com.recancheros.sportcenters.model.dto.SportCenterRequest;
 import com.recancheros.sportcenters.model.dto.SportCenterResponse;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SportCenterService {
     void addCenter(SportCenterRequest sportCenterRequest);
     Optional<SportCenterResponse> getSportCenterById(Long id) throws Exception;
+    List<SportCenterResponse> getAllSportCenters();
 }
