c3e969555174a2c5163c6acf2b487dd1090d02c6
Fabian Yate <fabianyate31@gmail.com>
2023-10-04 02:38:10 -0500

    Add role to the response and token

--- services/users-service/src/main/java/com/recancheros/users/services/impl/AuthServiceImpl.java
+++ services/users-service/src/main/java/com/recancheros/users/services/impl/AuthServiceImpl.java
@@ -4,8 +4,6 @@ import com.recancheros.users.config.jwt.JwtUtils;
 import com.recancheros.users.exceptions.AuthenticationFailedException;
 import com.recancheros.users.model.dto.UserRequest;
 import com.recancheros.users.model.dto.UserResponse;
-import com.recancheros.users.model.entity.User;
-import com.recancheros.users.model.mapper.UserMapper;
 import com.recancheros.users.services.AuthService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
@@ -20,14 +18,16 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService {
     private final Logger logger = LoggerFactory.getLogger(AuthServiceImpl.class);
     private final AuthenticationManager authenticationManager;
-    private final UserMapper userMapper;
     private final JwtUtils jwtUtils;
     private final UserServiceImpl userService;
 
     @Override
     public UserResponse authenticate(UserRequest userRequest) throws AuthenticationFailedException {
 
-        UserResponse userResponse;
+        UserResponse userResponse = new UserResponse();
+        String role = userService.getRoleByUserName(userRequest.getUsername());
+        final UserDetails userDetails = userService.loadUserByUsername(userRequest.getUsername());
+
         try {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
@@ -39,12 +39,9 @@ public class AuthServiceImpl implements AuthService {
             throw new AuthenticationFailedException();
         }
 
-        final UserDetails userDetails = userService.loadUserByUsername(userRequest.getUsername());
-
-        User user = userMapper.toEntity(userRequest);
-        userResponse = userMapper.toUserResponse(user);
-
-        userResponse.setJwt(jwtUtils.generateToken(userDetails));
+        userResponse.setUsername(userRequest.getUsername());
+        userResponse.setJwt(jwtUtils.generateToken(userDetails, role));
+        userResponse.setRole(role);
 
         return userResponse;
     }
