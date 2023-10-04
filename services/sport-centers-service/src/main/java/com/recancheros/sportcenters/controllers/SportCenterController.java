0696d229e62e18b300cf7e1ec65e157863c99a53
Fabian Yate <fabianyate31@gmail.com>
2023-10-04 02:45:07 -0500

    Add list of all SportCenters

--- services/sport-centers-service/src/main/java/com/recancheros/sportcenters/controllers/SportCenterController.java
+++ services/sport-centers-service/src/main/java/com/recancheros/sportcenters/controllers/SportCenterController.java
@@ -4,14 +4,16 @@ import com.recancheros.sportcenters.model.dto.SportCenterRequest;
 import com.recancheros.sportcenters.model.dto.SportCenterResponse;
 import com.recancheros.sportcenters.model.entity.SportCenter;
 import com.recancheros.sportcenters.services.SportCenterService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/sportcenters")
+@RequestMapping("/sport-centers-service/api/sportcenters")
 public class SportCenterController {
     private final SportCenterService sportCenterService;
 
@@ -21,9 +23,14 @@ public class SportCenterController {
 
 
     @PostMapping
-    public ResponseEntity<Void> addCenter(@RequestBody SportCenterRequest sportCenterRequest) {
+    public ResponseEntity<Void> addSportCenter(@RequestBody SportCenterRequest sportCenterRequest) {
         sportCenterService.addCenter(sportCenterRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SportCenterResponse>> getAllSportCenters() throws Exception {
+        return new ResponseEntity<>(sportCenterService.getAllSportCenters(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
