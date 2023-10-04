da0b9158ccd182186bcfd176286f626d4a32c9a1
Fabian Yate <fabianyate31@gmail.com>
2023-10-04 02:41:02 -0500

    Add jwt config for sport-center microservice

---
+++ services/sport-centers-service/src/main/java/com/recancheros/sportcenters/config/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,57 @@
+package com.recancheros.sportcenters.config.jwt;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtils jwtTokenProvider;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException  {
+        String token = jwtTokenProvider.resolveToken(request);
+
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            Claims claims = jwtTokenProvider.extractAllClaims(token);
+            String username = claims.getSubject();
+            String role = (String) claims.get("role");
+
+            List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority(role));
+
+            UserDetails userDetails = User.builder()
+                    .username(username)
+                    .password("")
+                    .authorities(authorities)
+                    .build();
+
+            Authentication auth = new PreAuthenticatedAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ services/sport-centers-service/src/main/java/com/recancheros/sportcenters/config/jwt/JwtUtils.java
@@ -0,0 +1,76 @@
+package com.recancheros.sportcenters.config.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    private static final long ACCESS_TOKEN_EXPIRATION_MS = 15 * 60 * (long) 1000;
+    @Value("${jwt.secret}")
+    private String key;
+
+
+    // Método para generar un token JWT
+    public String generateToken(Authentication authentication) {
+        UserDetails user = (UserDetails) authentication.getPrincipal();
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + ACCESS_TOKEN_EXPIRATION_MS);
+
+        return Jwts.builder()
+                .setSubject(user.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(SignatureAlgorithm.HS512, key)
+                .compact();
+    }
+
+    // Método para resolver el token desde la solicitud
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7); // Quita "Bearer " para obtener el token
+        }
+        return null;
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    // Método para validar un token JWT
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(key.getBytes()).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            // El token es inválido o ha expirado
+            return false;
+        }
+    }
+
+    // Método para obtener la autenticación a partir del token
+    public Authentication getAuthentication(String token) {
+        String username = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody().getSubject();
+        // Aquí, puedes cargar los roles del usuario desde tu sistema de autenticación o bases de datos si es necesario.
+        // Luego, puedes configurar la autenticación con esos roles.
+        // Por ejemplo:
+        // List<GrantedAuthority> authorities = loadAuthoritiesByUsername(username);
+        // UserDetails userDetails = new User(username, "", authorities);
+        // return new UsernamePasswordAuthenticationToken(userDetails, "", authorities);
+        return null; // Implementa la carga de roles y UserDetails según tu sistema
+    }
+}
