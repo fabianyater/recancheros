20a56f25caa298897b1bf8daf943085fd1ea30c1
Fabian Yate <fabianyate31@gmail.com>
2023-10-04 02:44:45 -0500

    Add feign client

--- services/users-service/src/main/java/com/recancheros/users/feign/SportCenterClient.java
+++ services/users-service/src/main/java/com/recancheros/users/feign/SportCenterClient.java
@@ -3,10 +3,11 @@ package com.recancheros.users.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "sport-centers")
+@FeignClient(name = "sport-centers-service")
 public interface SportCenterClient {
 
-    @GetMapping("/api/sportcenters/{id}")
-    SportCenterDTO getSportCenterById(@PathVariable Long id);
+    @PostMapping("/sport-centers-service/api/sportcenters/")
+    SportCenterDTO addSportCenter();
 }
