d13f01db9ded219bbf34086183327d483fe8a8ad
Fabian Yate <fabianyate31@gmail.com>
2023-10-06 18:32:51 -0500

    Add fields microservice

---
+++ services/fields-service/src/test/java/com/recancheros/fieldsservice/FieldsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recancheros.fieldsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
